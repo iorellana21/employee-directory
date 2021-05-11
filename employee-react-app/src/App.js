f19b4d9def287c0f95abbad5c2799ffacf8d2ca5
Ivan Orellana Jr <iorellana21@gmail.com>
2021-05-10 21:33:13 -0400

    create component folders/header js

---
+++ employee-react-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+// import Search from './components/Search';
+// import Table from './components/EmployeeTable';
+// import Filters from './components/Filters'
+import API from './utils/Api'
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<>
+				<Header />
+			</>
+		)
+	}
+
+}
+
+export default App;
